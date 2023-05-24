130f8562f2651cda4a861e462680fb41c9c88bbd
Harrison <harrisonkungu@yahoo.com>
2023-05-24 15:30:26 +0300

    Started NFC for IOS

--- ios/Plugin/CoopNFCPlugin.swift
+++ ios/Plugin/CoopNFCPlugin.swift
@@ -15,4 +15,52 @@ public class CoopNFCPlugin: CAPPlugin {
             "value": implementation.echo(value)
         ])
     }
+
+    @objc func startScan(_ call: CAPPluginCall) {
+        guard NFCReaderSession.readingAvailable else {
+          call.reject("NFC is not available on this device.")
+          return
+        }
+
+        let tagReader = NFCISO7816TagReader(call: call)
+        tagReader.startSession()
+      }
+}
+
+
+class NFCISO7816TagReader: NSObject, NFCTagReaderSessionDelegate {
+
+  // Properties
+  var tagSession: NFCTagReaderSession?
+  var completion: ((String?, Error?) -> Void)?
+  var call: CAPPluginCall?
+
+  // Initiate the tag session
+  init(call: CAPPluginCall) {
+    self.call = call
+  }
+
+  func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
+    // Handle tag detection and session connection
+    // ...
+  }
+
+  func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: Error) {
+    // Handle session invalidation
+    // ...
+  }
+
+  // Read the ISO-DEP tag
+  func readISO7816tag(tag: NFCISO7816Tag) {
+    // Read tag data and call back to completion closure
+    // ...
+  }
+
+  // Start the ISO-DEP tag reading session
+  func startSession() {
+    tagSession = NFCTagReaderSession(pollingOption: [.iso15693, .iso14443, .iso18092], delegate: self, queue: nil)
+    tagSession?.alertMessage = "Hold your iPhone near the ISO-DEP tag."
+    tagSession?.begin()
+  }
+
 }
